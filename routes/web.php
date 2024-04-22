4fdd747cfc2fe3155a1f58e5f3207b40ddbca05f
ChiniakinD <147251677+ChiniakinD@users.noreply.github.com>
2024-04-23 00:11:11 +0300

    1)Придумал и реализовал верстку авторизации в личный кабинет. 2)Добавил на вёрстке визуальное представление и логотип. 3)Добавить необходимые роуты в web.php (роут /login).

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-use App\Http\Controllers\IndexController;
-use App\Http\Controllers\SuggestionController;
-use App\Http\Controllers\AdminController;
+    use Illuminate\Support\Facades\Route;
+    use Inertia\Inertia;
+    use App\Http\Controllers\SuggestionController;
+    use App\Http\Controllers\AuthController;
 
-Route::get('/', [IndexController::class, 'index'])->name('app');
+    Route::get('/', function () {
+        return view('layouts.index');
+    })->name('app');
 
-Route::group(['prefix' => 'suggestions'], function () {
-    Route::get('/', [SuggestionController::class, 'index'])->name('suggestion.show');
-    Route::post('/', [SuggestionController::class, 'store'])->name('suggestion.store');
-    Route::get('/filter', [SuggestionController::class, 'filter'])->name('suggestion.filter');
-});
+    Route::get('/suggestions-show', [SuggestionController::class, 'index'])->name('suggestions.show');
+    Route::get('/auth-show', [AuthController::class, 'index'])->name('auth.show');
 
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('/', [AdminController::class, 'index'])->name('admin');
-});
+    Route::post('/suggestion-submit', [SuggestionController::class, 'store'])->name('suggestion.submit');
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
+
+    Route::middleware([
+        'auth:sanctum',
+        config('jetstream.auth_session'),
+        'verified',
+    ])->group(function () {
+        Route::get('/dashboard', function () {
+            return Inertia::render('Dashboard');
+        })->name('dashboard');
+    });
+
